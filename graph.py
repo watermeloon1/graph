1ac86ee01e7d785ef88dbbd183dffa498870ff6c
WaterMeloon <ifkovics.barnabas@gmail.com>
2022-05-31 13:57:47 +0200

    Mouse rotation implementation and cube shape is available.

--- graph.py
+++ graph.py
@@ -1,60 +1,93 @@
-from math import sqrt, sin, cos, isclose
+from math import sqrt, sin, cos, isclose, pi, acos
+import string
 import node
 import random
 import pygame
 
-RADIUS = 300
+RADIUS = 250
 ORIGO = (0, 0, 0)
 
-def random_tuple():
-    x = float(random.randrange(-RADIUS, RADIUS))
-    y = float(random.randrange(-RADIUS, RADIUS))
-    z = float(random.randrange(-RADIUS, RADIUS))
-    return(x, y, z)
+def random_coordinate_C():    
+    size = round((2 * RADIUS) / (sqrt(3) * 2), 0)
 
-def random_coordinate():
-    tuple = random_tuple()
-    while RADIUS <= sqrt(pow(tuple[0], 2) + pow(tuple[1], 2) + pow(tuple[2], 2)):
-        tuple = random_tuple()
-    return tuple
+    x = random.randrange(-size, size)
+    y = random.randrange(-size, size)
+    z = random.randrange(-size, size)
+
+    return (x, y, z)
+
+def random_coordinate_S():
+    u = random.random()
+    v = random.random()
+    r = random.random() ** (1./3.)
+
+    theta = u * 2.0 * pi
+    phi = acos(2.0 * v - 1.0)
+    sin_theta = sin(theta)
+    cos_theta = cos(theta)
+    sin_phi = sin(phi)
+    cos_phi = cos(phi)
+
+    x = r * sin_phi * cos_theta * RADIUS
+    y = r * sin_phi * sin_theta * RADIUS
+    z = r * cos_phi * RADIUS
+
+    return (x, y, z)
 
 def distance_3D(node1 : tuple , node2 : tuple):       
     x = pow(node1[0] - node2[0], 2)
     y = pow(node1[1] - node2[1], 2)
     z = pow(node1[2] - node2[2], 2)
+    
     return sqrt(x + y + z)
 
 def distance_2D(node1 : tuple, node2 : tuple):
     x = pow(node1[0] - node2[0], 2)
     y = pow(node1[1] - node2[1], 2)
+    
     return sqrt(x + y)
 
 def on_sphere(coordinates : tuple, radius : float):
     return isclose(pow((coordinates[0] - ORIGO[0]), 2) + pow((coordinates[1] - ORIGO[1]), 2) + pow((coordinates[2] - ORIGO[2]), 2), pow(radius, 2))
 
 class Graph:
-    def __init__(self, number : int):
-        self.nodes = []
-        print("Generating nodes...")
+    def __init__(self, shape : string, number : int):
 
-        for i in range(number):
-            self.nodes.append(node.Node(random_coordinate()))
+        self.nodes = []
+        
+        if not (shape == "sphere" or shape == "cube"):
+            print("ERROR: invalid string argument for graph type!")
+            return
+
+        if shape == "sphere":
+            for i in range(number):
+                coordinate = random_coordinate_S()
+                self.nodes.append(node.Node(coordinate))
+        else:
+            for i in range(number):
+                coordinate = random_coordinate_C()
+                self.nodes.append(node.Node(coordinate))
 
         self.radius_11 = distance_3D(ORIGO, self.nodes[11].get_tuple())
     
+    def check_coordinates(self):
+        for i in self.nodes:
+            if not -RADIUS < i.position_x < RADIUS or not -RADIUS < i.position_y < RADIUS or not -RADIUS < i.position_z < RADIUS:
+                print("ERROR: failed to initalise points!")
+
     def add_node(self, node : node.Node):
         self.nodes.append(node)
 
     def distance(self, n1 : int , n2 : int):    
         node1 = (self.nodes[n1].position_x, self.nodes[n1].position_y, self.nodes[n1].position_z)
         node2 = (self.nodes[n2].position_x, self.nodes[n2].position_y, self.nodes[n2].position_z)
+        
         return distance_3D(node1, node2)
 
     def draw(self, screen : pygame.Surface, WINDOW_WIDTH : int, WINDOW_HEIGHT : int):
 
-        nodes_drawn = 0
-
         for i in self.nodes: 
+
             if i.position_z < 0:
                 alpha = (RADIUS - (abs(i.position_z) / 2)) / RADIUS
             elif i.position_z > 0:
@@ -63,21 +96,26 @@ class Graph:
 
             RGB = (255 - (alpha * (255 / 2)), 255 - (alpha * (255 / 2)), 255 - (alpha * (255 / 2)))
 
-            if i == self.nodes[11]:
+            if i == self.nodes[11] or i == self.nodes[12]:
                 pygame.draw.circle(screen, (255,0,0), ((WINDOW_WIDTH / 2 + i.position_x) , (WINDOW_HEIGHT / 2 + i.position_y)), 2 * alpha)
-                nodes_drawn += 1
                 continue
 
-            if pygame.draw.circle(screen, RGB , ((WINDOW_WIDTH / 2 + i.position_x) , (WINDOW_HEIGHT / 2 + i.position_y)), 2 * alpha):
-                nodes_drawn += 1
+            if not pygame.draw.circle(screen, RGB , ((WINDOW_WIDTH / 2 + i.position_x) , (WINDOW_HEIGHT / 2 + i.position_y)), 2 * alpha):
+                print("ERROR: could not draw node " + str(i) + "!")
+            
+        #TRACE
+        #print("X1: " + str(self.nodes[0].position_x) + " Y1: " + str(self.nodes[0].position_y) + " X2: " + str(self.nodes[1].position_x) + str(" Y2: " + str(self.nodes[1].position_y)))
+        
+        #EDGE
+        #pygame.draw.line(screen, (200,200,200) , (WINDOW_WIDTH / 2 + self.nodes[0].position_x, WINDOW_HEIGHT / 2 + self.nodes[0].position_y), (WINDOW_WIDTH / 2 +self.nodes[1].position_x, WINDOW_HEIGHT / 2 +self.nodes[1].position_y) )
 
-        if len(self.nodes) != nodes_drawn:
-            print("ERROR: could not draw " + str(len(self.nodes) - nodes_drawn) + " nodes!")
-
-    def rotate_X(self, length : float):
+    def rotate_X(self, param : float, type : string):
 
         for i in self.nodes:
-            theta =length / distance_3D(ORIGO, i.get_tuple())
+            
+            if type == "cl":
+                theta = param / distance_3D(ORIGO, i.get_tuple())
+            else: theta = param
 
             sin_theta = sin(theta)
             cos_theta = cos(theta)
@@ -88,10 +126,13 @@ class Graph:
             i.position_y = pos_y
             i.position_z = pos_z
 
-    def rotate_Y(self, length : float):
+    def rotate_Y(self, param : float, type : string):
 
         for i in self.nodes:
-            theta = length / distance_3D(ORIGO, i.get_tuple())
+
+            if type == "cl":
+                theta = param / distance_3D(ORIGO, i.get_tuple())
+            else: theta = param
 
             sin_theta = sin(theta)
             cos_theta = cos(theta)
@@ -102,10 +143,13 @@ class Graph:
             i.position_x = pos_x
             i.position_z = pos_z
 
-    def rotate_Z(self, length : float):
+    def rotate_Z(self, param : float, type : string):
 
         for i in self.nodes:
-            theta = length / distance_3D(ORIGO, i.get_tuple())
+            
+            if type == "cl":
+                theta = param / distance_3D(ORIGO, i.get_tuple())
+            else: theta = param
 
             sin_theta = sin(theta)
             cos_theta = cos(theta)
@@ -116,10 +160,28 @@ class Graph:
             i.position_x = pos_x
             i.position_y = pos_y
 
-    def rotate_3D(self, mouse_prevoius : tuple, mouse : tuple, screen : pygame.Surface, WINDOW_WIDTH : int, WINDOW_HEIGHT : int):
+    def rotate_3DM(self, previous_click : tuple, click : tuple, param_type : string, WINDOW_WIDTH : int, WINDOW_HEIGHT : int):
 
-        self.rotate_X(1)
-        self.rotate_Y(1)
-        self.rotate_Z(1)
+        if not (param_type == "cl" or param_type == "th"):
+            print("ERROR: invalid arguments for rotation!")
+            return
+        
+        if param_type == "th":
+            speed = 0.01
+        else: speed = 1
+        
+        mouse_x = speed * ((WINDOW_WIDTH - previous_click[1]) - (WINDOW_WIDTH - click[1]))
+        mouse_y = speed * ((WINDOW_HEIGHT - previous_click[0]) - (WINDOW_HEIGHT - click[0]))
 
-        self.draw(screen, WINDOW_WIDTH, WINDOW_HEIGHT)
+        self.rotate_X(mouse_x, param_type)
+        self.rotate_Y(mouse_y, param_type)
+
+    def rotate_3DA(self, param : tuple, param_type : string):
+
+        if not (param_type == "cl" or param_type == "th"):
+            print("ERROR: invalid arguments for rotation!")
+            return
+        
+        self.rotate_X(param, param_type)
+        self.rotate_Y(param, param_type)
+        self.rotate_Z(param, param_type)
--- main.py
+++ main.py
@@ -5,26 +5,20 @@ WINDOW_WIDTH = 720
 WINDOW_HEIGHT = 720
 
 pygame.init()
+
 resolution =(WINDOW_WIDTH, WINDOW_HEIGHT)
 screen = pygame.display.set_mode(resolution)
-pygame.display.set_caption('GRAPH')
 screen.fill((255,255,255))
+
+pygame.display.set_caption('GRAPH')
 pygame.display.update()
 
 def main():
 
-    g = graph.Graph(500)
-    g.draw(screen, WINDOW_WIDTH, WINDOW_HEIGHT)
+    g = graph.Graph("cube", 5000)
 
-    #n1 = node.Node((0,0,0))
-    #n2 = node.Node((1,1,5))
-    #g.add_node(n1)
-    #g.add_node(n2)
-
-    #pressed = False
     previous_click = None
     exit = False
-    stop = False
     while not exit:
 
         for event in pygame.event.get():
@@ -36,19 +30,21 @@ def main():
                 previous_click = pygame.mouse.get_pos()
 
             if pygame.mouse.get_pressed()[0] and previous_click != None:
-                #pygame.draw.rect(screen, (255, 255, 255), (0, 0, WINDOW_WIDTH, WINDOW_HEIGHT))
-                #g.rotate_3D(previous_click, pygame.mouse.get_pos(), screen, WINDOW_WIDTH, WINDOW_HEIGHT)
+                g.rotate_3DM(previous_click, pygame.mouse.get_pos(), "th", WINDOW_WIDTH, WINDOW_HEIGHT)
                 previous_click = pygame.mouse.get_pos()
-                stop = not stop
 
             if not pygame.mouse.get_pressed()[0] and previous_click != None:
                 previous_click = None
 
-        if not stop:
-            pygame.draw.rect(screen, (255, 255, 255), (0, 0, WINDOW_WIDTH, WINDOW_HEIGHT))
-            g.rotate_3D((0,0,0), (0,0,0), screen, WINDOW_WIDTH, WINDOW_HEIGHT)       
-            print("X: " + str(g.nodes[11].position_x) + " Y: " + str(g.nodes[11].position_y) + " Z: " + str(g.nodes[11].position_z) + " R: " + str(graph.distance_3D(g.nodes[11].get_tuple(), graph.ORIGO)) + " O: " + str(g.radius_11) + " " + str(graph.on_sphere(g.nodes[11].get_tuple(), g.radius_11)))
-    
+        pygame.draw.rect(screen, (255, 255, 255), (0, 0, WINDOW_WIDTH, WINDOW_HEIGHT))
+        g.draw(screen, WINDOW_WIDTH, WINDOW_HEIGHT)
+
+            #DISTANCE
+            #print(graph.distance_3D(g.nodes[11].get_tuple(), g.nodes[12].get_tuple()))
+           
+           #POSITION
+            #print("X: " + str(g.nodes[11].position_x) + " Y: " + str(g.nodes[11].position_y) + " Z: " + str(g.nodes[11].position_z) + " R: " + str(graph.distance_3D(g.nodes[11].get_tuple(), graph.ORIGO)) + " O: " + str(g.radius_11) + " " + str(graph.on_sphere(g.nodes[11].get_tuple(), g.radius_11)))
+
         pygame.display.update()
 
     pygame.quit()
